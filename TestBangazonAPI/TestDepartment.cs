9932d52b05231fd0e08054cb5854ccce902117a2
Prafullata Sonawane <prafullatass@gmail.com>
2019-05-14 11:45:21 -0500

    added test for query string

--- TestBangazonAPI/TestDepartment.cs
+++ TestBangazonAPI/TestDepartment.cs
@@ -13,12 +13,50 @@ namespace TestBangazonAPI
 {
     public class TestDepartment
     {
+
+        [Fact]
+        public async Task Test_Get_Single_Department()
+        {
+
+            using (var department = new APIClientProvider().Client)
+            {
+                var response = await department.GetAsync("/api/Department/2");
+
+                response.EnsureSuccessStatusCode();
+
+                string responseBody = await response.Content.ReadAsStringAsync();
+                var departmentResponse = JsonConvert.DeserializeObject<Department>(responseBody);
+
+                Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+                Assert.Equal("IT", departmentResponse.Name);
+                Assert.NotNull(departmentResponse);
+            }
+        }
         [Fact]
         public async Task Test_Get_All_Departments()
         {
             using (var client = new APIClientProvider().Client)
             {
-               var response = await client.GetAsync("/api/Department");
+                var response = await client.GetAsync("/api/Department");
+
+
+                string responseBody = await response.Content.ReadAsStringAsync();
+                var departments = JsonConvert.DeserializeObject<List<Department>>(responseBody);
+
+                /*
+                    ASSERT
+                */
+                Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+                Assert.True(departments.Count > 0);
+            }
+        }
+        // get all department with employees
+        [Fact]
+        public async Task Test_Get_All_Departments_with_Employees()
+        {
+            using (var client = new APIClientProvider().Client)
+            {
+                var response = await client.GetAsync("/api/Department?_include=employees");
 
 
                 string responseBody = await response.Content.ReadAsStringAsync();
@@ -32,28 +70,49 @@ namespace TestBangazonAPI
             }
         }
 
+        //with budget with gt
         [Fact]
-        public async Task Test_Get_Single_Department()
+        public async Task Test_Get_All_Departments_with_Filter_Budget()
         {
-
-            using (var department = new APIClientProvider().Client)
+            using (var client = new APIClientProvider().Client)
             {
-                var response = await department.GetAsync("/api/Department/1");
+                var response = await client.GetAsync("/api/Department?_filter=budget&_gt=300");
 
-                response.EnsureSuccessStatusCode();
 
                 string responseBody = await response.Content.ReadAsStringAsync();
-                var departmentResponse = JsonConvert.DeserializeObject<Department>(responseBody);
+                var departments = JsonConvert.DeserializeObject<List<Department>>(responseBody);
 
+                /*
+                    ASSERT
+                */
                 Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-                Assert.Equal("Marketing", departmentResponse.Name);
-                Assert.NotNull(departmentResponse);
+                Assert.True(departments.Count > 0);
+            }
+        }
+        //with budget with gt and with employees
+        [Fact]
+        public async Task Test_Get_All_Departments_with_Filter_Budget_and_employee()
+        {
+            using (var client = new APIClientProvider().Client)
+            {
+                var response = await client.GetAsync("/api/Department?_filter=budget&_gt=300&_include=employees");
+
+
+                string responseBody = await response.Content.ReadAsStringAsync();
+                var departments = JsonConvert.DeserializeObject<List<Department>>(responseBody);
+
+                /*
+                    ASSERT
+                */
+                Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+                Assert.True(departments.Count > 0);
             }
         }
 
 
+
         [Fact]
-        public async Task Test_Create_Update_And_Delete_Department()
+        public async Task Test_Create_And_Delete_Department()
         {
             using (var client = new APIClientProvider().Client)
             {
@@ -79,41 +138,50 @@ namespace TestBangazonAPI
                 Assert.Equal("TestDept", newDept.Name);
                 Assert.Equal(200, newDept.Budget);
 
-                //Checking Update
-                int TestId = newDept.Id;
-                Department UpdateToNewDept = new Department
-                {
-                    Name = "New Dept",
-                    Budget = 500
-                };
-                var newDeptJSON = JsonConvert.SerializeObject(UpdateToNewDept);
                 
-
-                response = await client.PutAsync(
-                    $"api/Department/{TestId}",
-                    new StringContent(newDeptJSON, Encoding.UTF8, "application/json")
-                );
-                response.EnsureSuccessStatusCode();
-                responseBody = await response.Content.ReadAsStringAsync();
-
-                Assert.Equal(HttpStatusCode.NoContent, response.StatusCode);
-
-               
-                  //  GET single department 
-                 
-                var getNewDept = await client.GetAsync($"/api/Department/{TestId}");
-                getNewDept.EnsureSuccessStatusCode();
-                
-                string getNewDeptBody = await getNewDept.Content.ReadAsStringAsync();
-                newDept = JsonConvert.DeserializeObject<Department> (getNewDeptBody);
-
-                Assert.Equal(HttpStatusCode.OK, getNewDept.StatusCode);
-                Assert.Equal("New Dept", newDept.Name);
 
                 //Checking Delete Part
                 var deleteResponse = await client.DeleteAsync($"/api/Department/{newDept.Id}");
                 deleteResponse.EnsureSuccessStatusCode();
                 Assert.Equal(HttpStatusCode.NoContent, deleteResponse.StatusCode);
+            }
+        }
+
+        [Fact]
+        public async Task Test_Update_Department()
+        {
+            using (var client = new APIClientProvider().Client)
+            {
+                //Checking Update
+                
+                Department UpdateToNewDept = new Department
+                {
+                    Name = "Customer Service",
+                    Budget = 500
+                };
+                var newDeptJSON = JsonConvert.SerializeObject(UpdateToNewDept);
+
+
+                var response = await client.PutAsync(
+                            $"api/Department/1",
+                            new StringContent(newDeptJSON, Encoding.UTF8, "application/json")
+                        );
+                response.EnsureSuccessStatusCode();
+                var responseBody = await response.Content.ReadAsStringAsync();
+
+                Assert.Equal(HttpStatusCode.NoContent, response.StatusCode);
+
+
+                //  GET single department 
+
+                var getNewDept = await client.GetAsync($"/api/Department/1");
+                getNewDept.EnsureSuccessStatusCode();
+
+                string getNewDeptBody = await getNewDept.Content.ReadAsStringAsync();
+                Department newDept = JsonConvert.DeserializeObject<Department>(getNewDeptBody);
+
+                Assert.Equal(HttpStatusCode.OK, getNewDept.StatusCode);
+                Assert.Equal("Customer Service", newDept.Name);
             }
         }
 
